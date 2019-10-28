131409c116db25e61b2773e7e8140423114a4f7f
Luc Hoang <hoangduongluc125@gmail.com>
2019-10-28 16:02:17 +0700

    sdf

--- jenkins/scripts/deliver.dev.sh
+++ jenkins/scripts/deliver.dev.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 # install aws cli
-pip3 install awscli --upgrade --user
+# pip3 install awscli --upgrade --user
 
-# aws configuration
-/usr/local/bin/aws configure set aws_access_key_id AKIATQSRVQW4E52VWO4N
-/usr/local/bin/aws configure set aws_secret_access_key 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
-/usr/local/bin/aws configure set default.region ap-southeast-1
+# # aws configuration
+# /usr/local/bin/aws configure set aws_access_key_id AKIATQSRVQW4E52VWO4N
+# /usr/local/bin/aws configure set aws_secret_access_key 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
+# /usr/local/bin/aws configure set default.region ap-southeast-1
 
-# aws remove old IMA data
-/usr/local/bin/aws s3 rm s3://luckaka125 --recursive
+# # aws remove old IMA data
+# /usr/local/bin/aws s3 rm s3://luckaka125 --recursive
 
-# aws copy new IMA data to S3
-/usr/local/bin/aws s3 cp dist s3://luckaka125 --recursive
+# # aws copy new IMA data to S3
+# /usr/local/bin/aws s3 cp dist s3://luckaka125 --recursive
+cat /Users/luchoang/demo/abc.txt
